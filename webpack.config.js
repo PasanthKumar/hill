1f79ac7af3321394a37f6bc51d131214fd249c32
PasanthKumar <40565060+PasanthKumar@users.noreply.github.com>
2021-08-31 19:29:06 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+const webpack = require('webpack')
+
+
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
+
+const dirApp = path.join(__dirname, 'app')
+const dirAssets = path.join(__dirname, 'assets')
+const dirStyles = path.join(__dirname, 'styles')
+const dirNode = 'node_modules'
